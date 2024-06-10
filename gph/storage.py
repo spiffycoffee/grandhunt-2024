3e905327257e1fe75a201031532aced2c8c3e845
Philip Xie <philip@spiffycoffee.com>
2024-06-10 00:48:39 -0700

    collectstatic command no longer keeps original unhashed files

--- gph/storage.py
+++ gph/storage.py
@@ -1,4 +1,19 @@
-from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
+import os
 
-class CustomStorage(ManifestStaticFilesStorage):
+from django.conf import settings
+from django.contrib.staticfiles.storage import ManifestFilesMixin, ManifestStaticFilesStorage
+
+
+class DeleteOriginalsMixin(ManifestFilesMixin):
+    def post_process(self, *args, **kwargs):
+        if not kwargs.get('dry_run'):
+            files = super().post_process(*args, **kwargs)
+            for name, hashed_name, processed in files:
+                print(name, hashed_name, processed)
+                # Delete copied over original file to prevent unhashed files from being served
+                os.remove(settings.STATIC_ROOT + '/' + name)
+                yield name, hashed_name, processed
+
+
+class CustomStorage(DeleteOriginalsMixin, ManifestStaticFilesStorage):
     patterns = ()
